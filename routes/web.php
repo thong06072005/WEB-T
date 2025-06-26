8b163d5c8369e801ee70824a21090f9f1987779d
Thong <thongkg19@gmail.com>
2025-06-26 10:32:22 +0700

    update ten trang

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Lien_He_Controller;
 use Illuminate\Support\Facades\Route;
 
 
-Route::view('/lienhe', 'Lien_He')->name('lienhe');
+Route::view('/Lien_He', 'Lien_He')->name('lienhe');
 Route::post('/store', [Lien_He_Controller::class, 'store'])->name('store');
-Route::view('/tintuc', 'Tin_Tuc')->name('tintuc');
+Route::view('/Tin_Tuc', 'Tin_Tuc')->name('tintuc');
 
